a03da1e4fed3dd8a295059d325f4fb650834cd6e
sshoupm <sofiashoup@gmail.com>
2025-05-09 00:02:31 -0700

    Estructura principal de las funciones necesarias para csv_to_json.py

---
+++ funciones/csv_to_json.py
@@ -0,0 +1,17 @@
+#Archivo con las funciones necesarias para convertir un archivo CSV a JSON
+import csv
+import json
+import os
+
+def leer_csv():
+    # Aquí estará la lectura del archivo CSV
+    
+    pass
+
+def construir_diccionario():
+    # Aquí se armará el diccionario a partir de los datos leídos
+    pass
+
+def guardar_json():
+    # Aquí se guardará el diccionario en un archivo JSON
+    pass

edfff7e28a8777d174e5e72aceb169d5b2a5ab7b
mmerph <mercedes160503@gmail.com>
2025-05-11 23:35:54 -0700

    levenshtein.py actualizado

--- funciones/levenshtein.py
+++ funciones/levenshtein.py
@@ -1,28 +1,28 @@
 import Levenshtein
 
-def levenshtein_distance(cadena1, cadena2):
+def distancia_levenshtein(cadena1, cadena2):
     """
     Calcula la distancia de Levenshtein entre dos cadenas.
     """
     if len(cadena1) < len(cadena2):
-        return levenshtein_distance(cadena2, cadena1)
+        return distancia_levenshtein(cadena2, cadena1)
 
     # Si una de las cadenas está vacía
     if len(cadena2) == 0:
         return len(cadena1)
 
     # Crear la matriz de distancias
-    previous_row = range(len(cadena2) + 1)
+    fila_anterior = range(len(cadena2) + 1)
     for i, c1 in enumerate(cadena1):
-        current_row = [i + 1]
+        fila_actual = [i + 1]
         for j, c2 in enumerate(cadena2):
-            insertions = previous_row[j + 1] + 1
-            deletions = current_row[j] + 1
-            substitutions = previous_row[j] + (c1 != c2)
-            current_row.append(min(insertions, deletions, substitutions))
-        previous_row = current_row
+            inserciones = fila_anterior[j + 1] + 1
+            eliminaciones = fila_actual[j] + 1
+            sustituciones = fila_anterior[j] + (c1 != c2)
+            fila_actual.append(min(inserciones, eliminaciones, sustituciones))
+        fila_anterior = fila_actual
 
-    return previous_row[-1]
+    return fila_anterior[-1]
 
 def es_similar_levenshtein(titulo1, titulo2, umbral=0.8):
     """
